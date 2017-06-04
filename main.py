92d442d3ab89c2d4b47bc03bfe63fc4fbae460e3
zahramajd <zahra.majdabadi95@gmail.com>
2017-06-04 22:40:31 +0430

    neural network has been added

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ data=pd.concat([x_train,y_train])
 
 
 # Drop repeated features
-data = data.drop(['hour_b', 'total'],axis=1)
+data = data.drop(['hour_b', 'total','customerAttr_b','state'],axis=1)
+x_train=x_train.drop(['hour_b', 'total','customerAttr_b','state'],axis=1)
+x_test=x_test.drop(['hour_b', 'total','customerAttr_b','state'],axis=1)
 
 
 # handle Imbalanced data problem
@@ -28,19 +30,24 @@ data = data.drop(['hour_b', 'total'],axis=1)
 # Decision tree
 from sklearn.tree import DecisionTreeClassifier
 dtc=DecisionTreeClassifier(max_depth=5)
-dtc.fit(x_train,y_train)
+dtc.fit(x_train, y_train)
 
 y_pred_dtc=dtc.predict(x_test)
 
 # Random forest
-
 from sklearn.ensemble import RandomForestClassifier
 rfc = RandomForestClassifier()
-rfc = rfc.fit(x_train, y_train)
+rfc = rfc.fit(x_train, y_train.values.ravel())
 
 y_pred_rfc = rfc.predict(x_train)
 
 # Neural network
+from sklearn.neural_network import MLPClassifier
+nn = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(5, 2), random_state=1)
+nn.fit(x_train,y_train.values.ravel())
 
+y_pred_nn=nn.predict(x_test)
 
 # Logistic regression
+from sklearn.linear_model import LogisticRegression
+
