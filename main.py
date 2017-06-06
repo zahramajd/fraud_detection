929d3867ec2451196f36d89ab97756bae13bf69a
zahramajd <zahra.majdabadi95@gmail.com>
2017-06-06 14:15:35 +0430

    Handle Imbalanced data problem

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-
+from imblearn.over_sampling import SMOTE
 
 # Load data
 x_train = pd.read_csv("data_fraud/X_train.csv")
@@ -26,37 +25,54 @@ data = pd.concat([x_train, y_train])
 # sns.heatmap(corr, vmax=.8, square=True)
 # plt.show()
 
-# Split data to validation and train
-train_percent=0.66
-validate_percent=0.33
-m = len(x_train)
-x_train=x_train[:int(train_percent*m)]
-x_validation=x_train[int(validate_percent*m):]
-y_train=y_train[:int(train_percent*m)]
-y_validation=y_train[int(validate_percent*m):]
-
-# Drop repeated and unnecessary features
-data = data.drop(['hour_b', 'total', 'customerAttr_b', 'zip'], axis=1)
-x_train = x_train.drop(['hour_b', 'total', 'customerAttr_b', 'zip'], axis=1)
-x_test = x_test.drop(['hour_b', 'total', 'customerAttr_b', 'zip'], axis=1)
 
 # Convert to dummies
-x=pd.get_dummies(x_train['state'])
-x_train=pd.concat([x_train,x],axis=1)
-x_train=x_train.drop(['state'], axis=1)
+m = len(x_train)
+n = len(x_test)
 
-x=pd.get_dummies(x_test['state'])
-x_test=pd.concat([x_test,x],axis=1)
-x_test=x_test.drop(['state'], axis=1)
+x = pd.concat([x_train, x_test])
+
+states = pd.get_dummies(x['state'])
+x = pd.concat([x, states], axis=1)
+
+x_train = x[0:m]
+x_test = x[m:m + n]
+
+# # Convert to dummies
+# x=pd.get_dummies(x_train['state'])
+# x_train=pd.concat([x_train, x],axis=1)
+#
+# # x=pd.get_dummies(x_test['state'])
+# x_test=pd.concat([x_test, x], axis=1)
+#
+# x_test.fillna('0')
+
+# Split data to validation and train
+train_percent = 0.66
+validate_percent = 0.33
+m = len(x_train)
+x_train = x_train[:int(train_percent * m)]
+x_validation = x_train[int(validate_percent * m):]
+y_train = y_train[:int(train_percent * m)]
+y_validation = y_train[int(validate_percent * m):]
+
+# Drop repeated and unnecessary features
+data = data.drop(['hour_b', 'total', 'customerAttr_b', 'zip', 'state'], axis=1)
+x_train = x_train.drop(['hour_b', 'total', 'customerAttr_b', 'zip', 'state'], axis=1)
+x_test = x_test.drop(['hour_b', 'total', 'customerAttr_b', 'zip', 'state'], axis=1)
+x_validation = x_validation.drop(['hour_b', 'total', 'customerAttr_b', 'zip', 'state'], axis=1)
+
 
 
 # Normalize data
-x_train = preprocessing.normalize(x_train, norm='l2')
-x_test = preprocessing.normalize(x_test, norm='l2')
+# x_train = preprocessing.normalize(x_train, norm='l2')
+#x_test = preprocessing.normalize(x_test, norm='l2')
+#x_validation = preprocessing.normalize(x_validation, norm='l2')
+
 
 # Handle Imbalanced data problem
-# TODO : Handle Imbalanced data problem
-
+sm = SMOTE(random_state=42)
+x_train, y_train = sm.fit_sample(x_train, y_train)
 
 # # Decision tree
 dtc = DecisionTreeClassifier(max_depth=5)
@@ -64,20 +80,21 @@ dtc.fit(x_train, y_train)
 
 y_pred_dtc = dtc.predict(x_test)
 
-# Random forest
-rfc = RandomForestClassifier()
-rfc = rfc.fit(x_train, y_train.values.ravel())
 
-y_pred_rfc = rfc.predict(x_train)
-
-# Neural network
-nn = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-nn.fit(x_train, y_train.values.ravel())
-
-y_pred_nn = nn.predict(x_test)
-
-# Logistic regression
-lr = LogisticRegression()
-lr.fit(x_train, y_train.values.ravel())
-
-y_pred_lr = lr.predict(x_test)
+# # Random forest
+# rfc = RandomForestClassifier()
+# rfc = rfc.fit(x_train, y_train)
+#
+# y_pred_rfc = rfc.predict(x_train)
+#
+# # Neural network
+# nn = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+# nn.fit(x_train, y_train)
+#
+# y_pred_nn = nn.predict(x_test)
+#
+# # Logistic regression
+# lr = LogisticRegression()
+# lr.fit(x_train, y_train)
+#
+# y_pred_lr = lr.predict(x_test)
