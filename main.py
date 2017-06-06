e7e15376ecae7d3c9e0d0e2d6be3f941b37767f5
zahramajd <zahra.majdabadi95@gmail.com>
2017-06-06 22:34:19 +0430

    measure metrics, recall score has been added

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn import preprocessing
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.linear_model import LogisticRegression
 from imblearn.over_sampling import SMOTE
-from sklearn.metrics import f1_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import recall_score
 from sklearn.metrics import roc_auc_score
+from sklearn.tree import DecisionTreeClassifier
 
 # Load data
 x_train = pd.read_csv("data_fraud/X_train.csv")
 y_train = pd.read_csv("data_fraud/Y_train.csv")
 x_test = pd.read_csv("data_fraud/X_test.csv")
-
 
 # Draw correlation matrix
 # data = pd.concat([x_train, y_train])
@@ -41,7 +36,6 @@ x = pd.concat([x, states], axis=1)
 x_train = x[0:m]
 x_test = x[m:m + n]
 
-
 # Split data to validation and train
 train_percent = 0.66
 validate_percent = 0.33
@@ -56,63 +50,65 @@ x_train = x_train.drop(['hour_b', 'total', 'customerAttr_b', 'zip', 'state'], ax
 x_test = x_test.drop(['hour_b', 'total', 'customerAttr_b', 'zip', 'state'], axis=1)
 x_validation = x_validation.drop(['hour_b', 'total', 'customerAttr_b', 'zip', 'state'], axis=1)
 
-
 # Normalize data
 # x_train = preprocessing.normalize(x_train, norm='l2')
-#x_test = preprocessing.normalize(x_test, norm='l2')
-#x_validation = preprocessing.normalize(x_validation, norm='l2')
+# x_test = preprocessing.normalize(x_test, norm='l2')
+# x_validation = preprocessing.normalize(x_validation, norm='l2')
 
 
 # Handle Imbalanced data problem
 sm = SMOTE(random_state=42)
 x_train, y_train = sm.fit_sample(x_train, y_train)
 
-
 # Decision tree
 dtc = DecisionTreeClassifier(max_depth=5)
 dtc.fit(x_train, y_train)
 y_predicted_validation_dtc = dtc.predict(x_validation)
+y_predicted_test_dtc = dtc.predict(x_test)
 
 print "- Decision tree -"
 print "F1_Score: " + str(f1_score(y_validation, y_predicted_validation_dtc, average='macro'))
 print "accuracy: " + str(accuracy_score(y_validation, y_predicted_validation_dtc))
 print "AUC: " + str(roc_auc_score(y_validation, y_predicted_validation_dtc))
-
-y_predicted_test_dtc = dtc.predict(x_test)
-
+print "recall: " + str(recall_score(y_validation, y_predicted_validation_dtc))
 
 # Random forest
 rfc = RandomForestClassifier()
 rfc = rfc.fit(x_train, y_train)
 y_predicted_validation_rfc = rfc.predict(x_validation)
+y_prediction_test_rfc = rfc.predict(x_train)
 
 print "- Random forest -"
 print "F1_Score: " + str(f1_score(y_validation, y_predicted_validation_rfc, average='macro'))
 print "accuracy: " + str(accuracy_score(y_validation, y_predicted_validation_rfc))
 print "AUC: " + str(roc_auc_score(y_validation, y_predicted_validation_rfc))
-y_prediction_test_rfc = rfc.predict(x_train)
+print "recall: " + str(recall_score(y_validation, y_predicted_validation_dtc))
 
 # Neural network
 # TODO: debug
-nn = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1,)
-nn.fit(x_train, y_train)
-y_predicted_validation_nn = nn.predict(x_validation)
+# nn = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+# nn.fit(x_train, y_train)
+# y_predicted_validation_nn = nn.predict(x_validation)
+# y_predicted_test_nn = nn.predict(x_test)
+#
+# print "- Neural network -"
+# print "F1_Score: " + str(f1_score(y_validation, y_predicted_validation_nn, average='macro'))
+# print "accuracy: " + str(accuracy_score(y_validation, y_predicted_validation_nn))
+# print "AUC: " + str(roc_auc_score(y_validation, y_predicted_validation_nn))
+# print "recall: " + str(recall_score(y_validation, y_predicted_validation_dtc))
 
-print "- Neural network -"
-print "F1_Score: " + str(f1_score(y_validation, y_predicted_validation_nn, average='macro'))
-print "accuracy: " + str(accuracy_score(y_validation, y_predicted_validation_nn))
-print "AUC: " + str(roc_auc_score(y_validation, y_predicted_validation_nn))
-
-y_prediction_test_nn = nn.predict(x_test)
-
+#
 # Logistic regression
 lr = LogisticRegression()
 lr.fit(x_train, y_train)
 y_predicted_validation_lr = lr.predict(x_validation)
+y_prediction_test_lr = lr.predict(x_test)
 
 print "- Logistic regression -"
 print "F1_Score: " + str(f1_score(y_validation, y_predicted_validation_lr, average='macro'))
 print "accuracy: " + str(accuracy_score(y_validation, y_predicted_validation_lr))
 print "AUC: " + str(roc_auc_score(y_validation, y_predicted_validation_lr))
+print "recall: " + str(recall_score(y_validation, y_predicted_validation_dtc))
 
-y_prediction_test_lr = lr.predict(x_test)
+
+# pd.DataFrame({'frud': y_predicted_test_nn}).to_csv('P2_submission.csv', index =False)
